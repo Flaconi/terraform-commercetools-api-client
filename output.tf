04793c2b46092c6646d89d3d2e621e09e45345b6
Novikov Sergey <sergey.novikov@flaconi.de>
2021-08-31 16:11:32 +0200

    Back compatibility with 1.0.0 (#2)
    * Change recourse name to be 1.0 compatible
    
    * Add versions
    
    * Fix formatting

--- output.tf
+++ output.tf
@@ -2,7 +2,7 @@ output "api_client" {
   description = "created api client through terraform"
   value = element(coalescelist(
     commercetools_api_client.protected,
-    commercetools_api_client.unprotected,
+    commercetools_api_client.api_client,
   ), 0)
   sensitive = true
 }
