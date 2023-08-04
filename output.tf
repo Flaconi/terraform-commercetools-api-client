2d30e2be50718f4ceddcbed3ae64b71d23f68748
Alexander Miehe <alexander.miehe@flaconi.de>
2023-08-04 13:04:12 +0200

    PLT-609 - Fix output (#12)
    * bring back single api_client output for protected clients

--- output.tf
+++ output.tf
@@ -1,3 +1,9 @@
+output "api_client" {
+  description = "created api client through terraform"
+  value       = commercetools_api_client.protected
+  sensitive   = true
+}
+
 output "api_clients" {
   description = "created api clients through terraform"
   value       = commercetools_api_client.api_clients
